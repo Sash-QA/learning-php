70d806a72bba00419d6a7ba7d0ff4e0bedf99d75
Alexander Rakitin <a.rakitin24@yandex.ru>
2025-02-26 16:06:56 +0500

    Project structuring and minor changes

--- section-1/08-include-require-files/index.php
+++ section-1/08-include-require-files/index.php
@@ -2,19 +2,19 @@
 <!-- Выражения include и require полностью копируют код из подключаемых файлов -->
 
 <?php
-include ('header.php');         // Если include не найдет файл, то выполнение кода продолжится
-include_once ('header.php');    // include_once подключит файл, только если он еще не использовался ранее
-require ('content.php');        // Если require не найдет файл, то выполнение кода будет прервано
-require_once ('content.php');   // Аналогично с include_once
+include('header.php');         // Если include не найдет файл, то выполнение кода продолжится
+include_once('header.php');    // include_once подключит файл, только если он еще не использовался ранее
+require('content.php');        // Если require не найдет файл, то выполнение кода будет прервано
+require_once('content.php');   // Аналогично с include_once
 ?>
 
 Hi! <br><br>                    <!-- Можно вставлять текст между php -->
 
 <?php
-include ('comments.php');
+include('comments.php');
 echo $a;                        // Переменные, объявленные в подключенных выше файлах, можно использовать в этом файле
 $b = 123;                       // Переменные, объявленные в этом файле, можно использовать в подключенных ниже файлах
-include ('footer.php');
+include('footer.php');
 ?>
 
 <!--
